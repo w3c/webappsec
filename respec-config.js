5bdd9312953e7c96b8e6f145f74861cc8ae470fb
Mike West <mkwst@google.com>
2012-10-26 13:56:37 +0200

    CSP 1.1: Set a reasonable example by defining a policy for the spec itself.

---
+++ respec-config.js
@@ -0,0 +1,70 @@
+var respecConfig = {
+    // specification status (e.g. WD, LCWD, NOTE, etc.). If in doubt use ED.
+    // Member-SUBM
+    specStatus:           "ED",
+
+    // the specification's short name, as in http://www.w3.org/TR/short-name/
+    shortName:            "CSP",
+
+    // if your specification has a subtitle that goes below the main
+    // formal title, define it here
+    // subtitle   :  "an excellent document",
+
+    // if you wish the publication date to be other than today, set this
+    // publishDate:  "2009-08-06",
+
+    // if the specification's copyright date is a range of years, specify
+    // the start date here:
+    copyrightStart: "2010",
+
+    // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
+    // and its maturity status
+    // previousPublishDate:  "1977-03-15",
+    // previousMaturity:  "WD",
+
+    // if there a publicly available Editor's Draft, this is the link
+    edDraftURI:           "http://dvcs.w3.org/hg/content-security-policy/raw-file/tip/csp-specification.dev.html",
+
+    // if this is a LCWD, uncomment and set the end of its review period
+    // lcEnd: "2009-08-05",
+
+    // if you want to have extra CSS, append them to this list
+    // it is recommended that the respec.css stylesheet be kept
+    extraCSS:             ["css/respec.css"],
+
+    // editors, add as many as you like
+    // only "name" is required
+    editors:  [
+        { name: "Brandon Sterne", url: "mailto:bsterne@mozilla.com",
+          company: "Mozilla Corporation", companyURL: "http://www.mozilla.com/" },
+        { name: "Adam Barth", url: "mailto:w3c@adambarth.com",
+          company: "Google, Inc.", companyURL: "http://www.google.com/" },
+        { name: "Mike West", url: "mailto:mkwst@google.com",
+          company: "Google, Inc.", companyURL: "http://www.google.com/" },
+    ],
+
+    // authors, add as many as you like. 
+    // This is optional, uncomment if you have authors as well as editors.
+    // only "name" is required. Same format as editors.
+
+    //authors:  [
+    //    { name: "Your Name", url: "http://example.org/",
+    //      company: "Your Company", companyURL: "http://example.com/" },
+    //],
+
+    // name of the WG
+    wg:           "Web Application Security Working Group",
+
+    // URI of the public WG page
+    wgURI:        "http://www.w3.org/2011/webappsec/",
+
+    // name (with the @w3c.org) of the public mailing to which comments are due
+    wgPublicList: "public-webappsec",
+
+    // URI of the patent status for this WG, for Rec-track documents
+    // !!!! IMPORTANT !!!!
+    // This is important for Rec-track documents, do not copy a patent URI from a random
+    // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
+    // Team Contact.
+    wgPatentURI:  "http://www.w3.org/2004/01/pp-impl/49309/status",
+};
